e03b9a9df12e508ad69abc5347342e5bfbc1e3e4
HonestlyDex <82985536+HonestlyDex@users.noreply.github.com>
2021-10-08 21:41:19 -0400

    Create imgconverter.js

---
+++ imgconverter.js
@@ -0,0 +1,1 @@
+javascript:var imageplace = prompt("Enter Image URL");javascript:(function(){Array.prototype.slice.call(document.querySelectorAll('img')).map(function(el){el.src = imageplace;});}())
