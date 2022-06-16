9e1639e680bb003a02830a3b1b8c4748a7833b1a
BinaryDex <82985536+BinaryDex@users.noreply.github.com>
2022-06-15 22:32:32 -0400

    Create coordalert.js

---
+++ coordalert.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Bdocument.addEventListener('click'%2C (event) %3D> %7Balert(%60Mouse X%3A %24%7Bevent.clientX%7D%2C Mouse Y%3A %24%7Bevent.clientY%7D%5Cnmade by dex <3%60)%3B%7D)%7D)()
